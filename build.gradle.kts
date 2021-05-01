0c4896fa3534019479b6aff6b2b24c930526435f
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2021-05-01 03:11:16 +0300

    Add publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,21 @@
+import ru.ileasile.kotlin.apache2
+import ru.ileasile.kotlin.developer
+import ru.ileasile.kotlin.githubRepo
+
 plugins {
     kotlin("jvm")
     kotlin("jupyter.api")
     id("com.github.johnrengelman.shadow")
+    id("ru.ileasile.kotlin.publisher")
 }
 
 group = "org.jetbrains.kotlinx"
 version = "1.0.0"
+extra.set("localPublicationsRepo", rootDir.toPath().resolve("build/artifacts"))
 
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
 }
 
 dependencies {
@@ -19,12 +26,45 @@ dependencies {
 tasks.shadowJar {
     archiveBaseName.set(rootProject.name)
     archiveClassifier.set("")
-    //mergeServiceFiles()
     into("resources/lib") {
         from("lib")
     }
 
     manifest {
         attributes(tasks.jar.get().manifest.attributes)
+    }
+}
+
+kotlinPublications {
+    fun prop(name: String) = project.findProperty(name) as? String?
+
+    sonatypeSettings(
+        prop("kds.sonatype.user"),
+        prop("kds.sonatype.password"),
+        "rdkit-jupyter project, v. ${project.version}"
+    )
+
+    signingCredentials(
+        prop("kds.sign.key.id"),
+        prop("kds.sign.key.private"),
+        prop("kds.sign.key.passphrase")
+    )
+
+    pom {
+        githubRepo("ileasile", "rdkit-jupyter")
+        inceptionYear.set("2021")
+        licenses {
+            apache2()
+        }
+        developers {
+            developer("ileasile", "Ilya Muradyan", "Ilya.Muradyan@jetbrains.com")
+        }
+    }
+
+    publication {
+        publicationName = "jar"
+        artifactId = "rdkit-jupyter"
+        description = "RDKit for Jupyter Notebooks"
+        packageName = artifactId
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,10 +4,12 @@ pluginManagement {
     val kotlinVersion: String by settings
     val jupyterApiVersion: String by settings
     val shadowJarVersion: String by settings
+    val libsPublisherVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion
         kotlin("jupyter.api") version jupyterApiVersion
         id("com.github.johnrengelman.shadow") version shadowJarVersion
+        id("ru.ileasile.kotlin.publisher") version libsPublisherVersion
     }
 }
